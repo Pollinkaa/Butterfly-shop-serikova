7eefe7aa928f7be242b978b1bd129a8bfbfbb945
Pollinkaa <ip_serikova@mail.ru>
2022-02-02 20:35:27 +0300

    коммит

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,18 @@
-console.log('script work!');
+$(document).ready(function(){
+   let a = 10;
+   console.log(a);
+  let isOpen = false;
+
+  $('.j-burger').on('click', function() {
+    // if (isOpen) {
+    //   $('.j-menu').slideUp();
+    //   isOpen = false;
+    // } else {
+    //   $('.j-menu').slideDown();
+    //   isOpen = true;
+    // }
+
+    $('.j-menu').slideToggle();
+  });
+
+});
